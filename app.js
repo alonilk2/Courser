91b8b1e81b04aef32c2d844754cb37b562d56ba9
alonilk2 <63236463+alonilk2@users.noreply.github.com>
2021-01-15 17:29:06 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());	
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 app.post('/signin', async (req, res) => {
