3c71c75ebfcaece174eb9003c6c8843651078e58
alonilk2 <63236463+alonilk2@users.noreply.github.com>
2021-01-16 22:36:26 +0200

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.json());	
 app.use(bodyParser.urlencoded({extended: true}));
-
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.setHeader('Access-Control-Allow-Methods', 'POST, GET, PATCH, DELETE, OPTIONS');
+    next();
+});
 app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
