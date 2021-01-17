7909cbda37cd135e3207ac0b656f98197b722554
alonilk2 <63236463+alonilk2@users.noreply.github.com>
2021-01-18 00:56:02 +0200

    Update app.js

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ const jwtMW = exjwt({
   secret: 'manyplacees are awsome 4now',
   algorithms: ['RS256']
 });
-app.get('/', (req, res) => {
+app.get('/*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
 })
 app.post('/signin', async (req, res) => {
