69a0250dc0cba2dd63a77252dc9a9dfe53566ee8
Alon Barenboim <alonilk2@gmail.com>
2022-07-09 16:16:56 +0300

    1

--- models/index.js
+++ models/index.js
@@ -31,9 +31,9 @@ Object.keys(db).forEach(modelName => {
   }
 });
 
-sequelize.sync({
-  force: true
-});
+// sequelize.sync({
+//   force: true
+// });
 
 db.sequelize = sequelize;
 db.Sequelize = Sequelize;
